2e2a910f7fcc316740b99cc83e52af0873175434
Ismail Uras <ismailuras09@gmail.com>
2024-01-08 13:23:31 +0300

    home page swiper added

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -4,7 +4,6 @@ import featuresData from "./featuresData";
 import safety from "../../public/images/icons/safety.png";
 import expert from "../../public/images/icons/expert.png";
 import soluiton from "../../public/images/icons/solution.png";
-import quality from "../../public/images/icons/quality.png";
 
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -15,19 +14,11 @@ const Features = () => {
   return (
     <>
       <section id="features" className="pb-10 md:pb-20 lg:pb-28">
-        <div className="container flex flex-col items-center py-10 md:py-20 lg:py-28">
-          <h1 className="my-5 border-b border-primary pb-2 text-center text-3xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight dark:text-white">
+        <div className="container flex flex-col items-center py-10 md:py-20 lg:pb-28">
+          <h1 className="my-5 border-b border-primary pb-2 text-center text-3xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:mb-28 md:text-5xl md:leading-tight dark:text-white">
             Neden Biz?
           </h1>
-          <div className="mb-12 text-center text-base !leading-relaxed text-body-color-dark sm:text-lg md:text-xl">
-            Firmamız, 1600 metrekarelik bir alanda hizmet vermektedir ve ISO
-            9001:2008 Kalite Standartları belgesine sahiptir. Yaklaşık 70 uzman
-            personelimizle sizlere en kaliteli hizmeti sunmak için çalışıyoruz.
-            Her aşamada titizlikle yaklaştığımız dizgi sürecimizle,
-            müşterilerimizin beklentilerini karşılamak ve aşmak için çaba sarf
-            ediyoruz.
-          </div>
-          <div className="flex flex-wrap items-center justify-between gap-5 py-10">
+          <div className="flex flex-wrap items-center justify-between gap-8 py-10">
             <div className="h-full w-full space-y-7 rounded-sm p-4 md:h-60 md:w-80">
               <Image
                 src={expert}
@@ -52,19 +43,6 @@ const Features = () => {
                 <h1 className="text-2xl font-medium">Çözüm Odaklı Yaklaşım</h1>
                 <p className="text-base !leading-relaxed text-body-color-dark">
                   Tasarımdan üretime kadar kapsamlı çözümler sunuyoruz.
-                </p>
-              </div>
-            </div>
-            <div className="h-full w-full space-y-7 rounded-sm p-4 md:h-60 md:w-80">
-              <Image
-                src={quality}
-                alt="Çözüm Odaklı"
-                className="rounded-full bg-white object-cover object-center p-1"
-              />
-              <div className="space-y-2">
-                <h1 className="text-2xl font-medium">Kalite Standartları</h1>
-                <p className="text-base !leading-relaxed text-body-color-dark">
-                  ISO 9001:2008 belgesi ile kalitemizi kanıtlıyoruz.
                 </p>
               </div>
             </div>
---
+++ components/Hero/heroData.ts
@@ -0,0 +1,24 @@
+import main1 from "/public/images/swiper/main1.jpg";
+import main2 from "/public/images/swiper/main2.jpg";
+import main3 from "/public/images/swiper/main3.jpg";
+
+export const heroData = [
+  {
+    id: 1,
+    title: "Teknolojiye Yön Veren PCB Üretimi",
+    text: "Gelişmiş üretim süreçlerimiz ve en son teknoloji ile donanmış tesislerimizle, projelerinize liderlik eden bir PCB üretimi sunuyoruz.",
+    img: main1,
+  },
+  {
+    id: 2,
+    title: "Hızlı ve Güvenilir Çözümler",
+    text: "Zamanın önemli olduğunun farkındayız. Hızlı üretim süreçlerimiz ve güvenilir teknik ekibimizle, projelerinizi beklediğinizden daha kısa sürede tamamlıyoruz.",
+    img: main2,
+  },
+  {
+    id: 3,
+    title: "Özelleştirilmiş PCB Tasarımları",
+    text: "Her projenin benzersiz olduğunu biliyoruz. Uzman tasarımcılarımızla işbirliği yaparak, ihtiyaçlarınıza özel çözümler sunan özelleştirilmiş PCB tasarımlarımızla fark yaratın.",
+    img: main3,
+  },
+];
